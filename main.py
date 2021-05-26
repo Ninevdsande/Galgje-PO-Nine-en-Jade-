608bc6fe29e741c31390a1468e8d637340972ae2
ninevdsande <>
2021-05-26 11:55:29 +0000

    begin van def(play)

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import random
 word_list = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
 
-def get word (word_list):
+def get word(word_list):
   word = random.choice(word_list)
+  return word.upper
+
+
+def play(word):
+  word_completion = "_" * len(word)
+  guessed = False
+  guessed_letters = []
+  guessed_words = []
+  tries = 5
+  print("Je gaat galgje spelen!")
