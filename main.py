73ad65b287a19b241c156cd537b82048594ed33d
ninevdsande <>
2021-06-07 14:30:34 +0000

    kleine verbeterngen bij def

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
 word_list = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
 
-def get word(word_list):
+def get_word():
   word = random.choice(word_list)
-  return word.upper
+  return word.upper()
 
+def display_hangman():
+  aap = 1
 
 def play(word):
   word_completion = "_" * len(word)
@@ -16,19 +18,18 @@ def play(word):
   print(display_hangman(tries))
   print(word_completion)
   print("\n")
+
   while not guessed and tries > 0:
     guess = input("kies een letter:").upper()
-      if guess in guessed_letters:
-        print("Je hebt deze letter al een keer geraden.")
-      elif guess not n word:
-        print(guess,"komt niet in het woord voor.")
-        tries -= 1
-        guessed_letters.append(guess)
-      else:
-        print("goed gekozen," guess, "komt in het woord voor!")
-   
-    if len(guess) == len(word)
+    if guess in guessed_letters:
+      print("Je hebt deze letter al een keer geraden.")
+    elif guess not in word:
+      print(guess,"komt niet in het woord voor.")
+      tries -= 1
+      guessed_letters.append(guess)
+    else:
+      print("goed gekozen," + guess + "komt in het woord voor!")
+      guessed_letters.append(guess)
+      word_as_list = list(word_completion)
 
-
-
-
+print(get_word())
