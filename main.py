98fcb562c82c01fe8be11653a49c8197118ff4d6
ninevdsande <74553220+Ninevdsande@users.noreply.github.com>
2021-06-09 11:47:21 +0000

    tekst aangepast deel 2

--- main.py
+++ main.py
@@ -8,14 +8,22 @@ def get_word():
 def display_hangman():
   aap = 1
 
+
 def play(word):
+  print("We spelen galgje, leuk dat je mee doet!")
+  print("Je wint door letters te raden en zo achter het woord te komen.")
+  print( )
+  print("Je hebt 5 levens en als deze levens op zijn ben je dood")
+  print("Laten we beginnen")
+  print( )
+  print("Het eerste woord heeft " + str(len(word)) + " letters")
+
   word_completion = "_" * len(word)
   guessed = False
   guessed_letters = []
   guessed_words = []
   tries = 5
-  print("Je gaat galgje spelen!")
-  print(display_hangman(tries))
+
   print(word_completion)
   print("\n")
 
@@ -28,10 +36,15 @@ def play(word):
       tries -= 1
       guessed_letters.append(guess)
     else:
-      print("goed gekozen," + guess + "komt in het woord voor!")
+      print("goed gekozen, " + guess + " komt in het woord voor!")
       guessed_letters.append(guess)
       word_as_list = list(word_completion)
 
+<<<<<<< HEAD
+play(get_word())
+=======
 print(get_word())
 
+
+>>>>>>> origin/master
 
