d929d54a6862b0be75fdc1b72b057830aba1162d
ninevdsande <74553220+Ninevdsande@users.noreply.github.com>
2021-06-11 20:01:51 +0000

    spaties tussen de woorden

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def play(word):
   print ()
   naam = input ('Hoe heet je? ')
   print ()
-  print ("Hallo,", naam,"We spelen galgje, leuk dat je mee doet!" )
+  print ("Hallo,", naam,"we spelen galgje, leuk dat je mee doet!" )
   print ()
   print("Je wint door letters te raden en zo achter het woord te komen.")
   print( )
@@ -36,7 +36,7 @@ def play(word):
   while not guessed and counter < 5:
     guess = input("kies een letter:").upper()
     if guess == word:
-      print("Goed gedaan! Je hebt het woord" + word + "goed geraden!")
+      print("Goed gedaan! Je hebt het woord " + word + " goed geraden!")
       print("Dit is het einde van het spel, klik op Run om het spel nog een keer te spelen!")
       break
 
@@ -46,7 +46,7 @@ def play(word):
       word_as_list = list(word_completion) 
      
     elif guess not in word:
-      print(guess,"komt niet in het woord voor.")
+      print(guess," komt niet in het woord voor.")
       tries -= 1
       guessed_letters.append(guess)
       print("Je hebt nog " + str(tries) + " beurten over")
@@ -95,7 +95,7 @@ _____|""")
      print("Je hebt deze letter al een keer geraden.")
 
   if counter ==5:
-    print("Jammer!Je hebt het woord niet geraden. Het woord was"+ word +"Volgende keer beter! Als je het nog een keer wilt proberen klik dan op run.")
+    print("Jammer!Je hebt het woord niet geraden. Het woord was "+ word +" Volgende keer beter! Als je het nog een keer wilt proberen klik dan op run.")
 
   
 play(get_word())
